7cd324be61aa17fef815284a140937c17599a1b3
Beatkey <emrecaal@gmail.com>
2023-05-18 21:34:24 +0300

    Done: game history endpoint created Todo: game players and games models relationship

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -1,5 +1,6 @@
 const Games = require("../models/games")
 const GamePlayers = require("../models/game_players")
+const jwt = require("jsonwebtoken");
 
 exports.spinHistory = (req, res) => {
     Games.spinHistory((err, data) => {
@@ -15,5 +16,41 @@ exports.spinHistory = (req, res) => {
             });
         }
         ;
+    });
+}
+
+exports.gameHistory = (req, res) => {
+    console.log(req.body)
+    const token = req.headers["x-access-token"];
+
+    if (!token) {
+        return res.status(403).send({
+            message: "No token provided!"
+        });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: "Unauthorized!"
+            });
+        }
+
+        console.log(decoded)
+
+        GamePlayers.findAll({
+            where: {
+                user_id: decoded.id
+            }
+        }).then(data => {
+            //console.log(data)
+            res.send(data)
+        }).catch(e => {
+            res.status(500).send({
+                error: e
+            })
+        })
+
+
     });
 }
--- src/routes/games.js
+++ src/routes/games.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const GamesController = require("../controllers/games")
 
 router.get('/spin-history', GamesController.spinHistory);
+router.get('/game-history', GamesController.gameHistory);
 
 module.exports = router;
